c4b2921ae736a61b89277c3b97285ec05ea5f151
andrewljohnson <andrew@gaiagps.com>
2016-02-26 00:53:21 -0800

    warning for no slack URL

--- helpcenter_to_pdf/create_and_post.sh
+++ helpcenter_to_pdf/create_and_post.sh
@@ -1,8 +1,12 @@
 export PYTHONPATH=../to_json:../localize
-
 python ZendeskPDFMaker.py create
-
 python ZendeskPDFMaker.py post
 
-curl -X POST -H 'Content-type: application/json' --data '{"text":"Manual generation finished."}' $1
+slack_url=$1
+if [[ -n "$slack_url" ]]; then
+curl -X POST -H 'Content-type: application/json' --data '{"text":"Manual generation finished."}' slack_url
+else
+echo "no slack url given for manual update notif"
+fi
+
 
