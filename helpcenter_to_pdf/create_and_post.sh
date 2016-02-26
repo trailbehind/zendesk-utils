3b7d18511b784847bd617e94880f7345068e3fe9
andrewljohnson <andrew@gaiagps.com>
2016-02-26 13:56:39 -0800

    update shell script to log better

--- helpcenter_to_pdf/create_and_post.sh
+++ helpcenter_to_pdf/create_and_post.sh
@@ -3,10 +3,8 @@ python ZendeskPDFMaker.py create
 python ZendeskPDFMaker.py post
 
 slack_url=$1
-if [[ -n "$slack_url" ]]; then
-curl -X POST -H 'Content-type: application/json' --data '{"text":"Manual generation finished."}' $slack_url
-else
+if [ $# -ne 1 ]; then
 echo "no slack url given for manual update notif"
+else
+curl -X POST -H 'Content-type: application/json' --data '{"text":"Manual generation finished."}' $slack_url
 fi
-
-
