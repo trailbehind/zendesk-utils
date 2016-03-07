eb6a361bd050f17300f6fa56303b7df46d222a77
andrewljohnson <andrew@gaiagps.com>
2016-03-06 20:23:34 -0800

    update sample project settings and add more features to the pdfer

--- localize/_sample_project_settings.py
+++ localize/_sample_project_settings.py
@@ -1,9 +1,13 @@
-# zendesk
+'''
+    zendesk config
+'''
 subdomain = 'your_zendesk_domain'                
 email = 'you@example.com'                       
 token = ''                 
 
-# gengo
+'''
+    gengo config
+'''
 GENGO_PUBLIC_KEY = ''
 GENGO_PRIVATE_KEY = ''
 GENGO_API_URL = 'http://api.gengo.com/v2/translate/'
@@ -15,8 +19,14 @@ if GENGO_DEBUG:
   GENGO_PRIVATE_KEY = ''
   GENGO_API_URL = 'http://sandbox.gengo.com/v2/translate/'
 
-# point these at your images to customize the PDF cover pages
+S3_BUCKET_FOR_MANUAL = ''
+S3_ACCESS_KEY_FOR_MANUAL = ''
+S3_SECRET_KEY_FOR_MANUAL = ''
 
+'''
+   settings for look and feel of cover
+   point these at your images to customize the PDF cover pages
+'''
 # 8.5 x 11 image
 BACKGROUND_IMAGE_PATH = '../data/images/background.png'
 
@@ -26,7 +36,13 @@ BANNER_IMAGE_PATH = '../data/images/logo-banner.jpg'
 # small square image (~72px)
 ICON_IMAGE_PATH = '../data/images/app-icon.png'
 
+'''
+    the object the code refers to a lot
+    configure this for your project
+'''
+
 DATA_CONFIG = {
+  category_blacklist_for_pdfs: [],
   proper_nouns: [],
 	included_categories: [],
 	included_sections: [],
